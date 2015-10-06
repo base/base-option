6429f87c80e1e364eea6a54849ebe27bf1269270
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-06 05:29:00 -0400

    100% test coverage

--- test.js
+++ test.js
@@ -10,7 +10,7 @@
 require('mocha');
 var assert = require('assert');
 var Base = require('base-methods');
-var option = require('./');
+var options = require('./');
 var App, app;
 
 describe('option', function () {
@@ -23,13 +23,51 @@ describe('option', function () {
   });
 
   it('should add the option method to the `app` prototype:', function () {
-    app.mixin('option', option);
+    app.use(options);
     assert.equal(typeof App.prototype.option, 'function');
   });
 
-  it('should set/get options on `app.options`', function () {
-    app.mixin('option', option);
+  it('should set options', function () {
+    app.use(options);
     app.option('a', 'b');
     assert.equal(app.options.a, 'b');
+  });
+
+  it('should get options', function () {
+    app.use(options);
+    app.option('a', 'b');
+    var a = app.option('a');
+    assert.equal(a, 'b');
+  });
+
+  it('should set an object', function () {
+    app.use(options);
+    app.option({c: 'd'});
+    var c = app.option('c');
+    assert.equal(c, 'd');
+  });
+
+  it('should throw an error when key is falsey', function (done) {
+    try {
+      app.use(options);
+      app.option(null);
+      done(new Error('expected an error'));
+    } catch(err) {
+      assert(err);
+      assert(err.message === 'expected a string or object.');
+      done();
+    }
+  });
+
+  it('should throw an error when key is not a string or object', function (done) {
+    try {
+      app.use(options);
+      app.option(9);
+      done(new Error('expected an error'));
+    } catch(err) {
+      assert(err);
+      assert(err.message === 'expected a string or object.');
+      done();
+    }
   });
 });
