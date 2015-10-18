02ad254f964b382133465c64175e012cc49b830a
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-18 14:55:24 -0400

    use option-cache

--- index.js
+++ index.js
@@ -7,26 +7,13 @@
 
 'use strict';
 
-var utils = require('./utils');
-
 module.exports = function option(app) {
-  app.mixin('option', function(key, value) {
-    this.options = this.options || {};
+  var Options = require('option-cache');
 
-    if (typeof key === 'string') {
-      if (arguments.length === 1) {
-        return utils.get(this.options, key);
-      }
-      utils.set(this.options, key, value);
-      this.emit('option', key, value);
-      return this;
-    }
+  this.options = this.options || {};
+  Options.call(this);
 
-    if (key == null || typeof key !== 'object') {
-      throw new TypeError('expected a string or object.');
-    }
-
-    this.visit('option', key);
-    return this;
-  });
+  for (var key in Options.prototype) {
+    app.mixin(key, Options.prototype[key]);
+  }
 };
